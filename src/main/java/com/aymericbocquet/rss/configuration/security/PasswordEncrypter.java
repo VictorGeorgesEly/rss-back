eebe04077c30a1ad80efc8f74a45ca62223c96d5
victorely <victor.ely@isep.fr>
2019-03-30 16:12:08 +0100

    Correction of encryption

--- src/main/java/com/aymericbocquet/rss/configuration/security/PasswordEncrypter.java
+++ src/main/java/com/aymericbocquet/rss/configuration/security/PasswordEncrypter.java
@@ -2,6 +2,7 @@ package com.aymericbocquet.rss.configuration.security;
 
 import com.aymericbocquet.rss.data.entity.User;
 import org.bouncycastle.crypto.digests.SHA512Digest;
+import org.bouncycastle.util.encoders.Base64;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -29,14 +30,9 @@ public class PasswordEncrypter {
         SHA512Digest sha512Digest = new SHA512Digest();
         byte[] passwordBytes = password.getBytes(StandardCharsets.UTF_8);
         sha512Digest.update(passwordBytes, 0, passwordBytes.length);
-
         byte[] digestPassword = new byte[sha512Digest.getDigestSize()];
         sha512Digest.doFinal(digestPassword, 0);
-        String encryptedPassword = "";
-        for (byte d : digestPassword) {
-            encryptedPassword += d;
-        }
-        return encryptedPassword;
+        return new String(Base64.encode(digestPassword));
     }
 
 }
